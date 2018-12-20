3618bf75cca096986c7d8e328d03e0d87c5727dc
abhishekhotti <abhishek.hotti@sjsu.edu>
2018-12-20 12:46:32 -0800

    updates

---
+++ js/coding.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '2002318635',
+        limit: 32,
+        resolution: 'standard_resolution',
+        accessToken: '2002318635.1677ed0.84c7b4eacfd64c158d452f5098b50b01',
+        sortBy: 'most-recent',
+        template: '<div class="col-lg-4 instaimg gallery"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+    });
+
+    userFeed.run();
+   
+    // This will create a single gallery from all elements that have class "gallery-item"
+    $('.gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+});
