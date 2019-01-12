e023a688f2e0e2e3e270e572a973a812c594133b
abhishekhotti <abhishek.hotti@sjsu.edu>
2019-01-11 18:36:43 -0800

    sending email section

--- js/custom.js
+++ js/custom.js
@@ -64,6 +64,18 @@ function openTab(evt, cityName) {
 
     document.getElementById('contactForm').reset();
 
+    var url = form.action;
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+      console.log(xhr.status, xhr.statusText);
+      console.log(xhr.responseText);
+    }
+    var encoded = Object.keys(data).map(function(k) {
+      return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+    }).join('&');
+    xhr.send(encoded);
     // When the user clicks the button, open the modal 
     modal.style.display = "block";
   }
